b7f9b58d88556d52af9a96e4898a6cce40968e63
devhan-hub <hanabduman@gmail.com>
2024-11-03 19:51:00 +0300

    using tailwind css for styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Input from './components/Input';
+import TodoList from './components/TodoList.jsx';
+import { Provider } from 'react-redux';
+import './index.css';
+import Sidebar from './components/side/Sidebar.js';
+import Main from './components/main/Main.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen flex ">
+    
+      <Sidebar/>
+      <Main/>
     </div>
   );
 }
