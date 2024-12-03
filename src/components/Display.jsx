ae1c4b42d01826d6612eecc55ac46931f8487786
devhan-hub <hanabduman@gmail.com>
2024-12-04 01:14:21 +0300

    displaying selected todo

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -3,12 +3,13 @@ import Checkbox from '@mui/material/Checkbox';
 import { CircleOutlined, CheckCircle, Repeat, Edit } from "@mui/icons-material"
 import { useDispatch, useSelector } from "react-redux";
 import { updateTodo, updateAllTask } from '../Redux/TasksAddSlice'
-import { format, isToday, isTomorrow, isYesterday, parseISO } from 'date-fns';
+import { format, isToday, isTomorrow, isYesterday, parseISO , startOfDay } from 'date-fns';
 import { styled } from '@mui/material/styles';
 import { Grid, Typography, ButtonBase, Container } from '@mui/material'
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import dayjs from "dayjs";
 
 const Img = styled('img')({
   margin: 'auto',
@@ -21,24 +22,36 @@ const Img = styled('img')({
 
 const getDateStatue = (dueDate) => {
   if (!dueDate) return '';
-  const parsedDate = parseISO(dueDate);
+  const date= dueDate.toDate()
+  const isoString = date.toISOString()
+  const parsedDate = parseISO(isoString);
   if (isToday(parsedDate)) return 'Today';
   if (isTomorrow(parsedDate)) return 'Tomorrow';
   if (isYesterday(parsedDate)) return 'Yesterday';
-  return format(parsedDate, 'E , M dd');
+  return format(parsedDate, 'dd/MM/yyyy');
+}
+
+const isOverDue = (dueDate) => {
+
+  const parseDueDate = dayjs(dueDate.toDate()); 
+
+  const today = dayjs().startOf('day'); 
+
+  return parseDueDate.isBefore(today, 'day'); 
+
 }
 
 
 const priorityColor = (value) => {
   switch (value) {
-    case 'low':
+    case 'extreme':
+      return 'text-[#ff0000]'
+      break;
+    case 'moderate':
       return 'text-[#32CD32]'
       break;
-    case 'medium':
+    case 'low':
       return 'text-[#a8d8f2]'
-      break;
-    case 'high':
-      return 'text-[#FF4500]'
       break;
     default:
       return 'text-[#1E90FF]'
@@ -73,7 +86,7 @@ export default function Display({ todo }) {
         padding: 6,
         flexGrow: 1,
         position:'relative',
-        height: '100vh',
+        height:'max-content',
         top: '0',
         backgroundColor: '#fff',
         ...theme.applyStyles('dark', {
@@ -88,21 +101,21 @@ export default function Display({ todo }) {
       <Grid container  spacing={4}  >
 
         <Grid item xs={12}>
-        <Typography gutterBottom variant="h5" component="div" className="font-bold capitalize text-left md:text-center">
-        {todo.title}
+        <Typography  variant="h5" component="div" className="font-bold capitalize text-left md:text-center">
+        {todo?.title}
        </Typography>
         </Grid>
 
         <Grid item xs container  spacing={4}  alignItems={'center'} >
 
           <Grid item >
-            <Img alt="complex" className='w-[200px]' src={`/${todo.image}`} />
+            <Img alt="complex" className='w-[200px]' src={`/${todo?.image}`} />
           </Grid>
 
           <Grid item gap={2}  >
-            <p className='text-[1.13em] capitalize '> priority: <span className={`${priorityColor(todo.priority)} text-lg lowercase`}>{todo.priority}</span></p>
-            <p className='text-[1.13em] capitalize '> Status: <span className={`${priorityColor(todo.priority)} text-lg lowercase`}>{todo.priority}</span></p>
-            <p className='text-[1.13em] capitalize text-black text-opacity-45'> DueDate: <span className={`${priorityColor(todo.priority)} text-lg lowercase`}>{todo.priority}</span></p>
+            <p className='text-[1.13em] capitalize '> priority: <span className={`${priorityColor(todo?.priority)} text-lg lowercase`}>{todo?.priority}</span></p>
+            <p className='text-[1.13em] capitalize '> Status: <span className={`${todo?.comleted?'text-green-500':'text-red-600'} text-lg lowercase`}>{todo?.completed?'Completed':'Incomplete'}</span></p>
+            <p className='text-[1.13em] capitalize text-black text-opacity-45'> DueDate: <span className={`${isOverDue(todo.dueDate)?'text-red-600 ':''} text-lg lowercase`}>{getDateStatue(todo.dueDate)}</span></p>
           </Grid>
 
         </Grid>
@@ -111,12 +124,12 @@ export default function Display({ todo }) {
           <Typography variant="h6" gutterBottom>
             Task Description
           </Typography>
-          <Typography>{todo.description}</Typography>
+          <Typography>{todo?.description}</Typography>
 
         </Grid>
-      </Grid>
+      </Grid> 
 
-      <div item container alignSelf={'self-end'} className='absolute bottom-6 right-0 p-4 space-x-4'>
+      <div item container alignSelf={'self-end'} className='flex justify-end gap-6 mt-6'>
       <IconButton sx={{backgroundColor:'#FF4B3F', color:'white' , scale:'1.1', '&:hover':{scale:'1' , backgroundColor:'#FF4B3F'} ,transition:'all' , transitionDuration:'.5s' }} className="transition-all duration-700">
       <DeleteIcon />
           </IconButton>
