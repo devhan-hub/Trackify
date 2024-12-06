faff64201470cd8248af5ca0f938115e25b1956a
devhan-hub <hanabduman@gmail.com>
2024-12-06 14:51:52 +0300

    decompose ALlCatagory  to three component for redabilty

---
+++ src/components/allCatagory/AddNewCatagory.js
@@ -0,0 +1,84 @@
+import { useEffect , useState } from "react";
+import TextField from '@mui/material/TextField';
+import { Dialog , DialogContent , DialogActions , DialogTitle , Button } from "@mui/material";
+import useGroupManager from "../../hooks/useGroupManager";
+import { toast } from "react-toastify";
+
+ const AddNewCatagory =({ open, setOpen, userId, catagory, isEdit })=> {
+    const { edit, add } = useGroupManager(userId)
+    const [catagoryName, setCatagoryName] = useState('');
+    const [catagoryId, setCatagoryId] = useState('')
+
+
+    useEffect(() => {
+        if (!isEdit) {
+            setCatagoryName('')
+        }
+
+        else {
+            const { name, id, ...rest } = catagory
+            setCatagoryName(name);
+            setCatagoryId(id)
+        }
+    }, [catagory, isEdit])
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const newCatagory = {
+            name: catagoryName,
+            task: [],
+        }
+        const success = isEdit
+            ? await edit(catagoryId, { ...catagory, name: catagoryName })
+            : await add(newCatagory);
+
+    if(success){
+        toast.success(isEdit ? 'Successfully edited' : 'Successfully added');
+        setOpen(false);
+        setCatagoryName('');
+        setCatagoryId('');
+    }
+    else { 
+        toast.error(isEdit?'Unable to edit':'unable to add')      
+        setOpen(false)
+        setCatagoryName('')
+        setCatagoryId('')
+    } }
+
+
+    return (
+        <>
+            <Dialog
+                open={open}
+                onClose={() => setOpen(false)}
+                PaperProps={{
+                    component: 'form',
+                }}
+            >
+                <DialogTitle>{isEdit ? 'ADD' : 'Edit'} New Catagory</DialogTitle>
+                <DialogContent>
+
+                    <TextField
+                        autoFocus
+                        required
+                        margin="dense"
+                        id="name"
+                        name="catagory"
+                        value={catagoryName}
+                        label="Catagory Name"
+                        type="name"
+                        fullWidth
+                        variant="standard"
+                        onChange={(e) => setCatagoryName(e.target.value)}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpen(false)}>CANCEL</Button>
+                    <Button onClick={handleSubmit}>{isEdit ? 'EDIT' : "ADD"}</Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+}
+export default  AddNewCatagory; 
---
+++ src/components/allCatagory/CatagoryGrid.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { Delete, Edit } from '@mui/icons-material';
+import { Dialog , DialogContent , DialogActions , DialogTitle , Button ,IconButton, Typography } from "@mui/material";
+import { toast } from 'react-toastify';
+import useGroupManager from '../../hooks/useGroupManager';
+
+const  CatagoryGrid = ({ catagory, onClick, setCatagory, setOpen, setIsEdit, userId }) => {
+    const {deleteGroupById} = useGroupManager(userId)
+    const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+
+    const handelDelete = async () => {
+
+        const success = await deleteGroupById(catagory.id)
+        
+        if(success){
+            toast.success('permantely deleted')
+            setOpenDeleteDialog(false)
+        }
+        else {
+            setOpenDeleteDialog(false)
+            toast.error('unabe to delete')
+        }
+    }
+    return (
+        <>
+            <Dialog
+                open={openDeleteDialog}
+                onClose={() => setOpenDeleteDialog(false)}>
+                <DialogTitle>Delete</DialogTitle>
+                <DialogContent>Do you want to delete the  '{catagory?.name}' catagory?</DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpenDeleteDialog(false)}>CANCEL</Button>
+                    <Button onClick={handelDelete}>Delete</Button>
+                </DialogActions>
+            </Dialog>
+
+            <div className=' card  h-[150px] w-[240px]  bg-[#ff6867]  rounded-tl-[30px]   relative border-black overflow-hidden'  >
+
+                <div className='box  inset-[2px] absolute rounded-[10px] m-auto  bg-white  '>
+                    <div className='icon absolute bg-[#ff6867]  size-[75px]  rounded-br-[50%]'>
+
+                        <div className='bg-white absolute p-[8px] rounded-tl-[30px] rounded-br-[30px] m-auto top-1 left-1'><img src="/Images/home.png" alt="" className='size-12' /></div>
+                    </div>
+
+                    <div className='content flex flex-col h-full w-full gap-2 items-center justify-center'>
+
+                        <Typography variant='h6' className='capitalize '>{catagory?.name} </Typography >
+                        <Button variant='contained' size='small' sx={{ backgroundColor: '#ff6867', borderRadius: '12px', paddingInline: '20px' }} onClick={() => onClick(catagory.id)} >Open</Button>
+
+                    </div>
+                    <IconButton aria-label="delete" sx={{ color: "#ff6867", position: 'absolute', top: '0', right: '0' }} className='absolute top-0 right-0'>
+
+                        <Edit onClick={() => { setCatagory(catagory); setOpen(true); setIsEdit(true) }} />
+                    </IconButton>
+                    <IconButton aria-label="delete" sx={{ color: "#ff6867", position: 'absolute', bottom: '0', left: '0' }}>
+                        <Delete onClick={() => setOpenDeleteDialog(true)} />
+                    </IconButton>
+                </div>
+            </div>
+
+
+
+        </>
+    );
+}
+
+export default CatagoryGrid
+
+
