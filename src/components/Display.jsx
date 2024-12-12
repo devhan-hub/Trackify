90f30123c8a82d0a81f6cf034662c84e4acd9394
devhan-hub <hanabduman@gmail.com>
2024-12-12 19:55:43 +0300

    correcting functinality

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,5 +1,5 @@
 
-import { format, isToday, isTomorrow, isYesterday, parseISO  } from 'date-fns';
+import { format, isToday, isTomorrow, isYesterday, parseISO } from 'date-fns';
 import { styled } from '@mui/material/styles';
 import { Grid, Typography, ButtonBase, Container } from '@mui/material'
 import IconButton from '@mui/material/IconButton';
@@ -7,10 +7,18 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import dayjs from "dayjs";
 import InputToDo from "./inputTask/InputToDo";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import DeleteDialog from "./DeleteDialog";
 import useTaskManager from '../hooks/useTaskManager'
 import { toast } from "react-toastify";
+import Drawer from '@mui/material/Drawer';
+import {useMediaQuery} from '@mui/material';
+import Divider from '@mui/material/Divider';
+import Box from '@mui/material/Box';
+import { CloseOutlined } from '@mui/icons-material';
+
+
+
 
 const Img = styled('img')({
   margin: 'auto',
@@ -23,7 +31,7 @@ const Img = styled('img')({
 
 const getDateStatue = (dueDate) => {
   if (!dueDate) return '';
-  const date= dueDate.toDate()
+  const date = dueDate.toDate()
   const isoString = date.toISOString()
   const parsedDate = parseISO(isoString);
   if (isToday(parsedDate)) return 'Today';
@@ -34,11 +42,11 @@ const getDateStatue = (dueDate) => {
 
 const isOverDue = (dueDate) => {
 
-  const parseDueDate = dayjs(dueDate.toDate()); 
+  const parseDueDate = dayjs(dueDate.toDate());
 
-  const today = dayjs().startOf('day'); 
+  const today = dayjs().startOf('day');
 
-  return parseDueDate.isBefore(today, 'day'); 
+  return parseDueDate.isBefore(today, 'day');
 
 }
 
@@ -61,102 +69,134 @@ const priorityColor = (value) => {
 }
 
 
-export default function Display({ todoId , allTask ,groupId , userId }) {
- const [isEdit , setIsEdit]=useState(false)
- const[todo,setTodo]= useState(null)
- const[openInputDialog , setOpenInputDialog]= useState(false)
- const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
- const {deleteTodoById} = useTaskManager(userId ,groupId)
+export default function Display({ todoId, groupId, userId, todo, open, setOpen }) {
+  const [isEdit, setIsEdit] = useState(false)
+  const [openInputDialog, setOpenInputDialog] = useState(false)
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+  const { deleteTodoById } = useTaskManager(userId, groupId)
 
 
- useEffect(()=>{
-   setTodo(allTask.find((todo) => todo.id === todoId))
 
- },[allTask , todoId])
 
- const handelDelete= async ()=>{
-  const success = await deleteTodoById(groupId , todoId)
-  if(success){
-    toast.success('permantely deleted')
-    setOpenDeleteDialog(false)
-}
-else {
-    setOpenDeleteDialog(false)
-    toast.error('unabe to delete')
-}
-     
- }
+  const handelDelete = async () => {
+    const success = await deleteTodoById(todoId)
+    if (success) {
+      toast.success('permantely deleted')
+      setOpenDeleteDialog(false)
+    }
+    else {
+      setOpenDeleteDialog(false)
+      toast.error('unabe to delete')
+    }
 
- if(todo) {
-  return (
-   
-    <Container
-      sx={(theme) => ({
-        p: 2,
-        margin: 'auto',
-        width: '100%',
-        borderWidth: 2,
-        padding: 6,
-        flexGrow: 1,
-        position:'relative',
-        height:'max-content',
-        top: '0',
-        backgroundColor: '#fff',
-        ...theme.applyStyles('dark', {
-          backgroundColor: '#1A2027',
-        }),
+  }
 
-      })}
 
-    >
-        <DeleteDialog handelDelete={handelDelete} content={`' ${todo.title}' Task`} open={openDeleteDialog} setOpen={setOpenDeleteDialog}/>
-        {isEdit && <InputToDo todo={todo} isEdit={isEdit} setIsEdit={setIsEdit} open={openInputDialog} setOpen={setOpenInputDialog} groupId={groupId}/>}
-        
 
-      <Grid container  spacing={4}  >
 
-        <Grid item xs={12}>
-        <Typography  variant="h5" component="div" className="font-bold capitalize text-left md:text-center">
-        {todo?.title}
-       </Typography>
-        </Grid>
+  const DrawerList = (
+    <Box sx={{ width: 300 }} role="presentation" onClick={() => setOpen(false)}>
 
-        <Grid item xs container  spacing={4}  alignItems={'center'} >
+      <Container
+        sx={(theme) => ({
+          p: 2,
+          margin: 'auto',
+          width: '100%',
+          borderWidth: 2,
+          padding: 6,
+          flexGrow: 1,
+          position: 'relative',
+          height: 'max-content',
+          top: '0',
+          backgroundColor: '#fff',
+          ...theme.applyStyles('dark', {
+            backgroundColor: '#1A2027',
+          }),
 
-          <Grid item >
-            <Img alt="complex" className='w-[200px]' src={`/${todo?.image}`} />
+        })}
+
+      >
+        <DeleteDialog handelDelete={handelDelete} content={`' ${todo.title}' Task`} open={openDeleteDialog} setOpen={setOpenDeleteDialog} />
+        {isEdit && <InputToDo todo={todo} isEdit={isEdit} setIsEdit={setIsEdit} open={openInputDialog} setOpen={setOpenInputDialog} groupId={groupId} />}
+
+
+        <Grid container spacing={4}  >
+
+          <Grid item xs={12}>
+            <Typography variant="h5" component="div" className="font-bold capitalize text-left md:text-center">
+              {todo?.title}
+            </Typography>
           </Grid>
 
-          <Grid item gap={2}  >
-            <p className='text-[1.13em] capitalize '> priority: <span className={`${priorityColor(todo?.priority)} text-lg lowercase`}>{todo?.priority}</span></p>
-            <p className='text-[1.13em] capitalize '> Status: <span className={`${todo?.comleted?'text-green-500':'text-red-600'} text-lg lowercase`}>{todo?.completed?'Completed':'Incomplete'}</span></p>
-            <p className='text-[1.13em] capitalize text-black text-opacity-45'> DueDate: <span className={`${isOverDue(todo.dueDate)?'text-red-600 ':''} text-lg lowercase`}>{getDateStatue(todo.dueDate)}</span></p>
+          <Grid item xs container spacing={4} alignItems={'center'} >
+
+            <Grid item >
+              <Img alt="complex" className='w-[200px]' src={`/${todo?.image}`} />
+            </Grid>
+
+            <Grid item gap={2}  >
+              <p className='text-[1.13em] capitalize '> priority: <span className={`${priorityColor(todo?.priority)} text-lg lowercase`}>{todo?.priority}</span></p>
+              <p className='text-[1.13em] capitalize '> Status: <span className={`${todo?.comleted ? 'text-green-500' : 'text-red-600'} text-lg lowercase`}>{todo?.completed ? 'Completed' : 'Incomplete'}</span></p>
+              <p className='text-[1.13em] capitalize text-black text-opacity-45'> DueDate: <span className={`${isOverDue(todo.dueDate) ? 'text-red-600 ' : ''} text-lg lowercase`}>{getDateStatue(todo.dueDate)}</span></p>
+            </Grid>
+
           </Grid>
 
+          <Grid item className="space-y-2 ">
+            <Typography variant="h6" gutterBottom>
+              Task Description
+            </Typography>
+            <Typography>{todo?.description}</Typography>
+
+          </Grid>
         </Grid>
 
-        <Grid item className="space-y-2 ">
-          <Typography variant="h6" gutterBottom>
-            Task Description
-          </Typography>
-          <Typography>{todo?.description}</Typography>
-
-        </Grid>
-      </Grid> 
-
-      <div item container alignSelf={'self-end'} className='flex justify-end gap-6 mt-6'>
-      <IconButton sx={{backgroundColor:'#FF4B3F', color:'white' , scale:'1.1', '&:hover':{scale:'1' , backgroundColor:'#FF4B3F'} ,transition:'all' , transitionDuration:'.5s' }} className="transition-all duration-700" onClick={()=> setOpenDeleteDialog(true)}>
-      <DeleteIcon />
+        <div item container alignSelf={'self-end'} className='flex justify-end gap-6 mt-6'>
+          <IconButton sx={{ backgroundColor: '#FF4B3F', color: 'white', scale: '1.1', '&:hover': { scale: '1', backgroundColor: '#FF4B3F' }, transition: 'all', transitionDuration: '.5s' }} className="transition-all duration-700" onClick={() => setOpenDeleteDialog(true)}>
+            <DeleteIcon />
           </IconButton>
-          <IconButton sx={{backgroundColor:'#FF4B3F', color:'white' , scale:'1.1', '&:hover':{scale:'1' , backgroundColor:'#FF4B3F'} ,transition:'all' , transitionDuration:'.5s' }} className="transition-all duration-700" onClick={()=>{setIsEdit(true) ;setOpenInputDialog(true)}}>
+          <IconButton sx={{ backgroundColor: '#FF4B3F', color: 'white', scale: '1.1', '&:hover': { scale: '1', backgroundColor: '#FF4B3F' }, transition: 'all', transitionDuration: '.5s' }} className="transition-all duration-700" onClick={() => { setIsEdit(true); setOpenInputDialog(true) }}>
             <EditIcon />
           </IconButton>
         </div>
 
-    </Container>
-  );}
+      </Container>
+    </Box>
+  )
+
+  const isXL = useMediaQuery("(min-width:1080px)");
+
+
+  return (
+    <div>
+      {isXL ? (
+        <Drawer
+          sx={{
+            width: 300,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: 300,
+            },
+          }}
+          variant="persistent"
+          anchor="right"
+          open={open}
+        >
+          
+          <IconButton onClick={()=>setOpen(false)}>
+          <CloseOutlined /> 
+          </IconButton>
+       
+        <Divider />
+          {DrawerList}
+        </Drawer>
+      ) : (
+      <Drawer open={open} onClose={() => setOpen(false)} anchor='right'>
+        {DrawerList}
+      </Drawer>
+    )}
+
+    </div>
+  );
+
 }
-
-
-
-
--- src/components/EachCatagory.js
+++ src/components/EachCatagory.js
@@ -1,20 +1,16 @@
-import { useEffect, useState } from "react";
-import {  useSelector } from "react-redux";
-import SideDrawer from './SideDrawer.jsx'
-import Navbar from './Navbar.js'
+import { useState } from "react";
 import { Box } from "@mui/material";
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import ButtonBase from '@mui/material/ButtonBase';
-import {  Add as AddIcon } from "@mui/icons-material";
-import { format, toDate } from 'date-fns';
+import { Add as AddIcon } from "@mui/icons-material";
+import { format } from 'date-fns';
 import TodoDisplay from './TodoDisplay.js'
 import InputToDo from "./inputTask/InputToDo.jsx";
 import Display from "./Display.jsx";
 import Tooltip from '@mui/material/Tooltip';
-import { useParams } from "react-router-dom";
-import useTaskManager from "../hooks/useTaskManager.jsx";
-
+import { useSelector } from "react-redux";
+import { selectUserId } from "../Redux/User.jsx";
 
 
 const TodayDate = () => {
@@ -22,90 +18,62 @@ const TodayDate = () => {
   return <div>{today}<span className=" text-black text-opacity-45">.Today</span></div>;
 };
 
-const EachCatagory = ({ userId }) => {
-  const { groupId } = useParams();
-  const {tasksInGroup:todos } = useTaskManager(userId,groupId)
-  const groups=useSelector((state)=>state.toDo.groups)
+const EachCatagory = ({ name, task }) => {
+  const userId = useSelector(selectUserId)
   const [openInputDialog, setOpenInputDialog] = useState(false)
-  const[name , setName]= useState('Untitled')
-  const [selectedTodoId, setSelectedTOdoId] = useState(
-    // todos?.length !== 0 ? todos[0].id : ''
-    ''
-  );
-
-
-  useEffect(()=>{
-     const selectedGroup=groups.find((group)=>group.id === groupId)
-     setName(selectedGroup?.name)
-  } ,[groupId])
-
-  if (!todos) {
-    return (
-      <div>LOading ...</div>
-    )
-  }
+  const [openDisplayDialog, setOpenDisplayDialog] = useState(false)
+  const [selectedTodo, setSelectedTodo] = useState(null)
+  const [selectedTodoId, setSelectedTOdoId] = useState(null)
 
 
   return (
     <>
-      <Navbar />
-      <Box height={70} />
-      <Box className='xl:flex'>
+      <Box className=" space-y-2 p-4 lg:p-3 w-full border-black border-opacity-30 border-2 h-full ">
 
-        <SideDrawer />
-
-        <Box className=' lg:flex gap-4 flex-grow p-4 min-h-screen' >
-
-          <Box className=" space-y-2 p-4 lg:p-3 w-full border-black border-opacity-30 border-2 h-full ">
-
-            <div className=' shadow-2xl bg-white p-4 space-y-4 min-h-screen'>
-              <Grid container spacing={2} >
-                <Grid item xs={8} className='space-y-2   '>
-                 <div className='flex items-center gap-2'> <img src="/Images/home.png" alt="" className='size-8' /> <span className="text-[#ff6867] capitalize text-xl font-Monsta font-bold">{name}</span></div>
-                  <div>{TodayDate()}</div>
-                </Grid>
+        <div className=' shadow-2xl bg-white p-4 space-y-4 min-h-screen'>
+          <Grid container spacing={2} >
+            <Grid item xs={8} className='space-y-2   '>
+              <div className='flex items-center gap-2'> <img src="/Images/home.png" alt="" className='size-8' /> <span className="text-[#ff6867] capitalize text-xl font-Monsta font-bold">{name}</span></div>
+              <div>{TodayDate()}</div>
+            </Grid>
 
             <Grid item>
-                <ButtonBase onClick={() => setOpenInputDialog(true)} className="w-full ">
-                  <Tooltip title='Add new task  '>
-                    <AddIcon className="text-[#ff6867] w-full" /> Add Task
-                    </Tooltip>
-                  </ButtonBase>
-                  </Grid>
-              </Grid>
-              <InputToDo open={openInputDialog} setOpen={setOpenInputDialog} groupId={groupId} isEdit={false}/>
+              <ButtonBase onClick={() => setOpenInputDialog(true)} className="w-full ">
+                <Tooltip title='Add new task  '>
+                  <AddIcon className="text-[#ff6867] w-full" /> Add Task
+                </Tooltip>
+              </ButtonBase>
+            </Grid>
+          </Grid>
+          <InputToDo open={openInputDialog} setOpen={setOpenInputDialog} groupId={'4task'} isEdit={false} />
 
 
-              {(todos?.length === 0 && todos )? (
-                <div className='h-full w-full flex flex-col items-center justify-center p-12'>
-                  <img src="/Images/home.png" alt="" className='size-32' />
-                  <Typography variant="h6">NO Task</Typography >
-                </div>
-              ) : (
-                <Grid container alignItems={'center'} justifyItems={'center'} spacing={3} className="w-full">
-                  {todos?.map((todo, index) => {
-                    const isSelected = selectedTodoId === todo.id;
-
-                    return (
-                      <Grid item key={index} className={`w-full cursor-pointer `} onClick={() => setSelectedTOdoId(todo.id)}>
-                        <TodoDisplay todo={todo} all={true} isSelected={isSelected} />
-                      </Grid>
-                    )
-                  })}
-                </Grid>
-              )}
+          {(task?.length === 0 && task) ? (
+            <div className='h-full w-full flex flex-col items-center justify-center p-12'>
+              <img src="/Images/home.png" alt="" className='size-32' />
+              <Typography variant="h6">NO Task</Typography >
             </div>
+          ) : (
+            <Grid container alignItems={'center'} justifyItems={'center'} spacing={3} className="w-full">
+              {task?.map((todo, index) => {
+                const isSelected = selectedTodoId === todo.id;
 
-          </Box>
-
-
-          <Box item md={6} sx={12} className="sticky min-h-screen h-max top-[10px] border-black border-opacity-30 border-2  p-4 pb-6 self-start  w-full   ">
-            <Display todoId={selectedTodoId} allTask={todos} groupId={groupId} userId={userId} className=' shadow-2xl bg-white  ' />
-          </Box >
-
-        </Box>
+                return (
+                  <Grid item key={index} className={`w-full cursor-pointer `} onClick={() => { setSelectedTOdoId(todo.id); setSelectedTodo(todo); setOpenDisplayDialog(true) }}>
+                    <TodoDisplay todo={todo} all={true} isSelected={isSelected} />
+                  </Grid>
+                )
+              })}
+            </Grid>
+          )}
+        </div>
 
       </Box>
+
+
+        { selectedTodo && <Display todoId={selectedTodoId} todo={selectedTodo} groupId={'4task'} userId={userId} open={openDisplayDialog} setOpen={setOpenDisplayDialog} className=' shadow-2xl bg-white  ' />
+        }         
+
     </>
   );
 }
