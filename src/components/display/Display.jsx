c58a9e5591caf8a532419949e0181fa00abeb549
devhan-hub <hanabduman@gmail.com>
2024-12-13 23:11:52 +0300

    task display components

---
+++ src/components/display/Display.jsx
@@ -0,0 +1,152 @@
+
+import { styled } from '@mui/material/styles';
+import {  Typography, Container } from '@mui/material'
+import IconButton from '@mui/material/IconButton';
+import Grid from '@mui/material/Grid2';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+import InputToDo from "../inputTask/InputToDo";
+import { useState } from "react";
+import DeleteDialog from "../DeleteDialog";
+import useTaskManager from '../../hooks/useTaskManager'
+import { toast } from "react-toastify";
+import Drawer from '@mui/material/Drawer';
+import Divider from '@mui/material/Divider';
+import { CloseOutlined} from '@mui/icons-material';
+import { priorityColor ,isOverDue ,getDateStatus}  from './Utiles'
+
+const Img = styled('img')({
+  margin: 'auto',
+  display: 'block',
+  maxWidth: '100%',
+  maxHeight: '100%',
+  borderRadius: '10px'
+});
+
+export default function Display({ todoId, groupId, userId, todo, open, setOpen }) {
+  const [isEdit, setIsEdit] = useState(false)
+  const [openInputDialog, setOpenInputDialog] = useState(false)
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+  const { deleteTodoById } = useTaskManager(userId)
+
+
+  const handelDelete = async () => {
+    const success = await deleteTodoById(todoId,groupId)
+    if (success) {
+      toast.success('permantely deleted')
+      setOpenDeleteDialog(false)
+    }
+    else {
+      setOpenDeleteDialog(false)
+      toast.error('unabe to delete')
+    }
+
+  }
+
+  const DrawerList = (
+
+      <Container
+        sx={(theme) => ({
+          width: '100%',
+          borderWidth: 2,
+          flexGrow: 1,
+          height: 'max-content',
+          top: '0',
+          backgroundColor: '#fff',
+          ...theme.applyStyles('dark', {
+            backgroundColor: '#1A2027',
+          }),
+     
+
+        })}
+      >
+        <DeleteDialog handelDelete={handelDelete} content={`' ${todo.title}' Task`} open={openDeleteDialog} setOpen={setOpenDeleteDialog} />
+        {isEdit && <InputToDo todo={todo} isEdit={isEdit} setIsEdit={setIsEdit} open={openInputDialog} setOpen={setOpenInputDialog} groupId={groupId} />}
+
+         
+        <Grid container spacing={3} >
+        <Grid size={12} height={35}/>
+
+          <Grid  size={12}>
+            <Typography variant="h5" component="div" className="font-bold capitalize text-center ">
+              {todo?.title}
+            </Typography>
+          </Grid>
+
+
+            <Grid size={12} >
+              <Img alt="complex" className='w-[200px]' src={`/${todo?.image}`} />
+            </Grid>
+
+            <Grid size={12} gap={2}  >
+              <p className=' capitalize '> priority: <span className={`${priorityColor(todo?.priority)}  lowercase`}>{todo?.priority}</span></p>
+              <p className=' capitalize '> Status: <span className={`${todo?.comleted ? 'text-green-500' : 'text-red-600'}  lowercase`}>{todo?.completed ? 'Completed' : 'Incomplete'}</span></p>
+              <p className=' capitalize text-black text-opacity-45'> DueDate: <span className={`${isOverDue(todo.dueDate) ? 'text-red-600 ' : ''}  lowercase`}>{getDateStatus(todo.dueDate)}</span></p>
+            </Grid>
+
+
+          <Grid size={12} className="space-y-2 ">
+            <Typography variant="h6" gutterBottom>
+              Task Description
+            </Typography>
+            <Typography>{todo?.description}</Typography>
+          </Grid>
+        </Grid>
+
+        <div item container alignSelf={'self-end'} className='flex justify-end gap-6 mt-6'>
+          <IconButton sx={{ backgroundColor: '#FF4B3F', color: 'white', scale: '1.1', '&:hover': { scale: '1', backgroundColor: '#FF4B3F' }, transition: 'all', transitionDuration: '.5s' }} className="transition-all duration-700" onClick={() => setOpenDeleteDialog(true)}>
+            <DeleteIcon />
+          </IconButton>
+          <IconButton sx={{ backgroundColor: '#FF4B3F', color: 'white', scale: '1.1', '&:hover': { scale: '1', backgroundColor: '#FF4B3F' }, transition: 'all', transitionDuration: '.5s' }} className="transition-all duration-700" onClick={() => { setIsEdit(true); setOpenInputDialog(true) }}>
+            <EditIcon />
+          </IconButton>
+        </div>
+        <Grid size={12} height={10}/>
+
+      </Container>
+
+  )
+
+  return (
+    <div>
+     
+        <Drawer
+          sx={{
+            width: 400,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: 400,
+            },
+          }}
+          variant="persistance"
+          anchor="right"
+          open={open}
+          onClose={()=>setOpen(false)}
+          className='xl:block hidden'
+        >
+          
+          <IconButton onClick={()=>setOpen(false)}>
+          <CloseOutlined /> 
+          </IconButton>
+       
+        <Divider />
+          {DrawerList}
+        </Drawer>
+     
+      <Drawer
+       sx={{
+        width: 400,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: 400,
+        },
+      }}
+       className='xl:hidden block' open={open} variant='temporary' onClose={() => setOpen(false)} anchor='right'>
+        {DrawerList}
+      </Drawer>
+  
+    </div>
+  );
+
+}
---
+++ src/components/display/TodoDisplay.js
@@ -0,0 +1,101 @@
+import Paper from "@mui/material/Paper"
+import Checkbox from '@mui/material/Checkbox';
+import { CircleOutlined, CheckCircle } from "@mui/icons-material"
+import { useDispatch, useSelector } from "react-redux";
+import { styled } from '@mui/material/styles';
+import { Typography, ButtonBase, IconButton } from '@mui/material'
+import Grid from "@mui/material/Grid2";
+import { selectUserId } from "../../Redux/User.jsx";
+import useTaskManager from "../../hooks/useTaskManager.jsx";
+import Display from "./Display.jsx";
+import { useState } from "react";
+import { priorityColor ,isOverDue ,getDateStatus} from './Utiles.jsx'
+
+const Img = styled('img')({
+  margin: 'auto',
+  display: 'block',
+  maxWidth: '100%',
+  maxHeight: '100%',
+  borderRadius: '10px'
+});
+
+
+export default function TodoDisplay({ todo, groupId, isSelected, all }) {
+  const userId = useDispatch(selectUserId);
+  const { edit } = useTaskManager(userId)
+  const [openDisplayDialog, setOpenDisplayDialog] = useState(false)
+
+
+
+
+  return (
+    <Paper
+      sx={(theme) => ({
+        p: 2,
+        margin: 'auto',
+        width: '100%',
+        height: 'auto',
+        borderRadius: '15px',
+        flexGrow: 1,
+        backgroundColor: '#fff',
+        ...theme.applyStyles('dark', {
+          backgroundColor: '#1A2027',
+        }),
+      })}
+    >
+
+      <Display todoId={todo.id} todo={todo} groupId={groupId} userId={userId} open={openDisplayDialog} setOpen={setOpenDisplayDialog} className=' shadow-2xl bg-white  ' />
+
+      <Grid container spacing={0} columnSpacing={0} >
+        <Grid size={2}   >
+          <IconButton onClick={() => {
+            edit(todo.id, { ...todo, completed: !todo.completed } ,groupId);
+          }}>
+            <Checkbox checked={todo.completd} icon={<CircleOutlined />} checkedIcon={<CheckCircle />}
+              sx={{
+                color: 'red',
+                '&.Mui-checked': {
+                  color: 'green',
+                }
+              }}
+            />
+          </IconButton>
+        </Grid>
+        <Grid container  size={10} justifyContent={'flex-start'} alignItems={'center'} onClick={()=>setOpenDisplayDialog(true)}>
+        <Grid size={12}>
+          <Typography gutterBottom variant="h6" className="font-bold capitalize">
+            {todo.title}
+          </Typography>
+        </Grid>
+       
+        <Grid size={8}>
+          <Typography variant="body2" gutterBottom>
+            {todo.description.length > 50 ? todo.description.slice(0, 100) + '...' : todo.description}
+          </Typography>
+        </Grid>
+        <Grid size={4}>
+          <ButtonBase sx={{ width: 128, height: 128 }}>
+            <Img alt="complex" src={`${todo.image}`} />
+          </ButtonBase>
+        </Grid>
+        
+        <Grid size={4}>
+          <Typography sx={{ cursor: 'pointer' }} variant="body2" >
+            priority: <span className={`${priorityColor(todo.priority)}`}>{todo.priority}</span>
+          </Typography>
+        </Grid>
+        <Grid size={4} className=''>DueDate:{getDateStatus(todo.dueDate)}
+
+        </Grid>
+        <Grid size={4} className="capitalize  ">
+          Status: <span className={` ${todo.completd ? 'text-green-500' : 'text-red-600'} font-bold`}> {todo.completd ? 'completed' : 'incomplete'}</span></Grid >
+          </Grid>
+      </Grid>
+
+
+    </Paper>
+  );
+}
+
+
+
