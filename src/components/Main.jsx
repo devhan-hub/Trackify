f518de72d8af60bd7adc0baec62252d28a2bdab0
devhan-hub <hanabduman@gmail.com>
2024-12-13 23:21:58 +0300

    display each group task

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,46 +1,62 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import EachCatagory from './EachCatagory'
 import { useSelector } from 'react-redux'
 import { todayTask, vitalTask } from '../Redux/TasksAddSlice'
 import { selectUserId } from '../Redux/User'
 import useTaskManager from '../hooks/useTaskManager'
+import { Box, CircularProgress, Typography } from '@mui/material'
+import { PriorityHigh } from '@mui/icons-material'
 
-const Main = ({group}) => {
+const Main = ({group , name}) => {
     const todaytask= useSelector(todayTask)
     const vitaltask=useSelector(vitalTask)
 
     switch(group){
         case 'myday':
-            return <EachCatagory task={todaytask} name={group}/>
+            return <EachCatagory task={todaytask} name={group} groupId={group}/>
         case 'vitalTask':
-            return <EachCatagory task= {vitaltask} name={group}/>  
+            return <EachCatagory task= {vitaltask} name={group} groupId={group}/>  
         case 'planned' :
             // return <Planned/>  
            default :
-             <CreatedGroup groupId={group}/>   
+           return  <CreatedGroup groupId={group} name={name}/>   
     }
  
 }
 
-export const CreatedGroup=({groupId})=>{
+export const CreatedGroup=({groupId ,name})=>{
     const userId = useSelector(selectUserId)
-    const {tasksInGroup:todos } = useTaskManager(userId,groupId)
+    const {fetch } = useTaskManager(userId)
+
+    useEffect(()=>{
+      fetch(groupId)
+    },[fetch,groupId])
+
+    const todos= useSelector((state)=>state.toDo.tasksByGroup[groupId])
     const todosByGroupStatus=useSelector((state)=>state.toDo.todosByGroupStatus[groupId])
 
     if (todosByGroupStatus === 'loading') {
         return (
-          <div>Loading ...</div>
+          <Box sx={{ display: 'flex',flexDirection:'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <CircularProgress />
+        </Box>
         )
       }
     
       if (todosByGroupStatus === 'failed') {
         return (
-          <div>OOPS unable to load</div>
-        )
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+          <PriorityHigh color="error" />
+          <Typography color="error" variant="h6">
+            Failed to load data. Please try again later.
+          </Typography>
+        </Box>        )
       }
  
     return (
-        <EachCatagory task={todos} name={groupId}/>
+      <>
+        <EachCatagory task={todos} name={name} groupId={groupId}/>
+        </>
     )
  
 }
