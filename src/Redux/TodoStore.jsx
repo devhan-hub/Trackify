b35f5df29e52262945d25e13bc97c6abd331b3c7
devhan-hub <hanabduman@gmail.com>
2024-12-13 23:23:20 +0300

    removing NavStore

--- src/Redux/TodoStore.jsx
+++ src/Redux/TodoStore.jsx
@@ -1,12 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit'
 import todoReducer from './TasksAddSlice'
 import userReducer from './User';
-import navReducer from './NavStore'
  const store = configureStore({
     reducer:{
         toDo:todoReducer,
         user:userReducer,
-        open:navReducer
     }
  })
 
